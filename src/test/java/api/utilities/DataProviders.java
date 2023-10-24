bdc88a73246429f19d841b21f58a078c8c4fbc6a
danial <danila.bashlykoff@yandex.ru>
2023-10-24 17:02:04 +0300

    this is my first commit

---
+++ src/test/java/api/utilities/DataProviders.java
@@ -0,0 +1,54 @@
+package api.utilities;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+	@DataProvider(name="Data")
+	public String[][] getAllData() throws IOException
+	{
+		String path = System.getProperty("user.dir")+"\\testData\\Userdata.xlsx";
+		
+		XLUtility xl = new XLUtility(path);
+		
+		int rownum = xl.getRowCount("Лист1");
+		int colcount = xl.getCellCount("Лист1", 1);
+		
+		String apidata[][] = new String[rownum][colcount];
+		
+		for(int i=1; i<=rownum; i++)
+		{
+			for (int j=0; j<colcount; j++)
+			{
+				apidata[i-1][j] = xl.getCellData("Лист1", i, j);
+			}
+		}
+		
+		return apidata;
+		
+	}
+	
+	
+	@DataProvider(name="UserNames")
+	public String[] getUsersNames() throws IOException
+	{
+		
+		String path = System.getProperty("user.dir")+"\\testData\\Userdata.xlsx";
+		XLUtility xl = new XLUtility(path);
+		
+		int rownum = xl.getRowCount("Лист1");
+		
+		String apidata[] = new String[rownum];
+		
+		for(int i=1; i<=rownum; i++)
+		{
+			apidata[i-1] = xl.getCellData("Лист1", i, 1);
+		}
+		
+		return apidata;
+		
+	}
+	
+}
